6a740cb78dd2e439b0ba2562b9dfd61ae5b4fdfe
Halyna Sinko <galinasinko14@gmail.com>
2021-06-08 00:22:55 +0300

    Добавляет основной файл скрипта.
    Функции создают разметке по шаблонам, рендерят ее и обрабатывают ошибки.

---
+++ src/js/main.js
@@ -0,0 +1,58 @@
+const debounce = require('lodash.debounce');
+
+import fetchCountries from './fetchCountries';
+import countriesListTpl from '../templates/countriesTpl.hbs';
+import countryTpl from '../templates/countryTpl.hbs';
+
+import { onFetchError, onFetchSuccess } from './notification';
+
+const refs = {
+  inputFied: document.querySelector('input'),
+  countryContainer: document.querySelector('.js-container'),
+};
+
+// console.log(refs.countryContainer);
+
+refs.inputFied.addEventListener('input', debounce(onInputChange, 500));
+
+function onInputChange(event) {
+  refs.countryContainer.innerHTML = '';
+  const searchQuery = event.target.value.trim();
+  // console.log(searchQuery);
+  if (!searchQuery) {
+    return;
+  }
+
+  fetchCountries(searchQuery).then(renderMarkup).catch(onFetchError);
+}
+
+function renderMarkup(data) {
+  if (data.status === 404) {
+    onFetchError(`${data.message}. Incorrect query.`);
+    return;
+  }
+
+  if (data.length > 1 && data.length < 11) {
+    const markupContries = countriesListTpl(data);
+    insertMarkup(markupContries);
+    // console.log(data.length);
+    return;
+  }
+
+  if (data.length === 1) {
+    const markupContry = countryTpl(data);
+    insertMarkup(markupContry);
+    onFetchSuccess();
+    return;
+  }
+
+  if (data.length > 10) {
+    onFetchError('Too many matches found. Please enter a more specific query!');
+    return;
+  }
+  // console.log(data);
+}
+
+function insertMarkup(tpl) {
+  refs.countryContainer.insertAdjacentHTML('beforeend', tpl);
+}
