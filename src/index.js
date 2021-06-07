9b9757ce568eb93d8d23320faddfc81da7cdbd4e
Halyna Sinko <galinasinko14@gmail.com>
2021-06-08 00:17:22 +0300

    Добавляет импорт  PNotify.css и js файлa.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './sass/main.scss';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/mobile/dist/PNotifyMobile.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+import './js/main.js'
